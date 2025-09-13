ac9a84011e41b06079451b96da27f24b40043803
[bisha21] <[timilsinab22bi@gmail.com]>
2025-09-13 19:40:43 +0545

    `Added Socket.IO functionality to the server, including user socket mapping and real-time messaging.`

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,16 +1,17 @@
 import express, { Request, Response } from 'express';
 import dotenv from 'dotenv';
-import authRoutes from './routes/auth.routes'
+import authRoutes from './routes/auth.routes';
+import messageRoutes from './routes/message.routes';
 dotenv.config();
 
-const app= express();
+const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use('/api/auth',authRoutes )
-app.get('/', (req:Request, res:Response) => {
-    res.json({message: 'hello world'})
-})
+app.use('/api/auth', authRoutes);
+app.use('/api/message', messageRoutes);
+app.get('/', (req: Request, res: Response) => {
+  res.json({ message: 'hello world' });
+});
 
-
-export default app
+export default app;
